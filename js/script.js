133bc1bb5a76c9dee4536b163df9c01fc353d09d
Marco Rehberg <rehberg@ewerk.de>
2018-10-29 15:30:41 +0100

    Masonry Code einstellen - Alles Sites und Dateien

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+(function (window, document, $) {
+    $(function() {
+// ----------------------------------------------
+
+$('.masonry-grid').masonry({
+    // options
+    itemSelector: '.masonry-grid-item',
+    // columnWidth: 200
+  });
+      
+
+// ----------------------------------------------
+    });
+}(window, document, jQuery));
